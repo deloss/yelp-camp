3a1cd428feef387ba41a3c2efe034dc0405c496e
Santiago de los Santos <santiago.delossantos1998@gmail.com>
2020-01-31 11:44:39 +0000

    Data persistence added to campgrounds

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://deloss:password1234@cluster0-zimcv.mongodb.net/test?retryWrites=true&w=majority');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 
-var campgrounds = [{
-	name: 'Campground example',
-	image: 'https://s3.amazonaws.com/pdugallery2/23539/big_DSC04353_.jpg'
-},
-{
-	name: 'Beach houes',
-	image: 'https://s3.amazonaws.com/pdugallery2/23539/big_01-DSC00026.jpg'
-}];
+var campgroundSchema = new mongoose.Schema({
+	title: String,
+	imageUrl: String
+});
+
+var CampgroundModel = mongoose.model("Campground", campgroundSchema);
 
 app.listen(3000, '0.0.0.0', () => {
 	console.log("App has started.")
@@ -23,15 +24,30 @@ app.get('/', (req, res) => {
 })
 
 app.get('/campgrounds', (req, res) => {
-	res.render('campgrounds', {campgrounds});
+	CampgroundModel.create({title: "hola", imageUrl: "urll..."})
+	var campgrounds = CampgroundModel.find({} , (error, camp) => {
+		if(!error) {
+			console.log(camp);
+			res.render('campgrounds', {campgrounds: camp});
+		} else {
+			res.send("There was an error");
+		}
+	});
+	
 })
 
 app.post('/campgrounds', (req, res) => {
 	var newCampgroundTitle = req.body.title; 
 	var newCampgroundImageUrl = req.body.image;
 	if(newCampgroundTitle && newCampgroundImageUrl) {
-		campgrounds.push({name: newCampgroundTitle, image: newCampgroundImageUrl});
-		res.redirect('/campgrounds');
+		CampgroundModel.create({title: newCampgroundTitle, imageUrl: newCampgroundImageUrl}, (error, camp) => {
+			if(!error) {
+				res.redirect('/campgrounds');
+			} else {
+				res.send("There was an error");
+			}
+		});
+		
 	} else {
 		res.send('There was an error')
 	}
