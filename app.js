8c00f062c01ffe96c354f9b48ff69da25f90f32b
Santiago de los Santos <santiago.delossantos1998@gmail.com>
2020-02-03 18:50:38 +0000

    Comments added and some design done

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var CampgroundModel = require('./models/campground');
+var Comment = require('./models/comments');
 var seedsDB = require('./seeds');
 
-seedsDB();
+//seedsDB();
 
 mongoose.connect('mongodb+srv://deloss:password1234@cluster0-zimcv.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser : true, useUnifiedTopology: true});
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static(__dirname + '/public'));
 
 
 
@@ -51,20 +53,49 @@ app.post('/campgrounds', (req, res) => {
 	}
 })
 
+app.get('/campgrounds/new', (req, res) => {
+	res.render('new-campground');
+})
+
+
 app.get('/campgrounds/:id', (req, res) => {
 	CampgroundModel.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
 		if(err) {
 			console.log("The campground doesnt exist");
 			res.send("The campground doesnt exist");
 		} else {
+			console.log(foundCampground);
 			res.render("campground-details", {campground: foundCampground});
 		}
 	})
 })
 
-app.get('/campgrounds/new', (req, res) => {
-	res.render('new-campground');
+app.post('/campgrounds/:id/comments', (req, res) => {
+	CampgroundModel.findById(req.params.id, (err, campgroundFound) => {
+		if(err) {
+			res.send('There was an error')
+			console.log(err);
+		}
+		else {
+			console.log(req.body.comment);
+			Comment.create({
+				text: req.body.comment, 
+				author: 'Jorge'
+			}, function(err, comment){
+				if(err){
+					console.log(err);
+				} else {
+					campgroundFound.comments.push(comment);
+					campgroundFound.save();
+					console.log("Created new comment");
+				}
+			})
+			console.log(campgroundFound);
+			res.redirect(`/campgrounds/${req.params.id}`);
+		}
+	})
 })
+
 
 // MOBILE SECTION
 
